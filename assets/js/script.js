0c4b87595c837aa735dde5fd02d324a86867b64f
Wilson Acosta <sayhi@wilsonacosta.us>
2017-11-15 20:39:50 -0600

    JS SUbmit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,23 @@
 $("#submit").on('click', function() {
+	e.preventDefault();
 
+	var data = {
+		name: $('#name').val().trim(),
+		phoneNumber: $('#phoneNumber').val().trim(),
+		email: $('email').val().trim(),
+		uniqueID: $('uniqueID').val().trim()
+	}
+
+
+	$.ajax({
+		type: 'POST',
+		data: JSON.stringify(data),
+	    contentType: 'application/json',
+	    url: '/api/new',						
+	    success: function(data) {
+	        console.log('success');
+	        console.log(JSON.stringify(data));
+	    }
+    })
 
 })
