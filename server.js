cd559ad628a667af44fbd56d11539f0e0522096a
omes000 <omes000@gmail.com>
2017-11-15 19:19:34 -0600

    updating server.js

--- server.js
+++ server.js
@@ -8,12 +8,19 @@ var PORT = 3000;
 var haveTables = [];
 var waitingList = [];
 
-app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/", function(req, res) {
 	res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/view", function(req, res) {
+	res.sendFile(path.join(__dirname, "view.html"));
+});
+
+app.get("/make", function(req, res) {
+	res.sendFile(path.join(__dirname, "make.html"));
 });
 
 app.listen(PORT, function() {
