eedf6989a8b5a11aa842e93e4886d6dae7acbc35
omes000 <omes000@gmail.com>
2017-11-15 20:59:46 -0600

    updating most files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,24 @@
-$("#submit").on('click', function() {
-	e.preventDefault();
 
-	var data = {
-		name: $('#name').val().trim(),
-		phoneNumber: $('#phoneNumber').val().trim(),
-		email: $('email').val().trim(),
-		uniqueID: $('uniqueID').val().trim()
-	}
+		$("#submit").on('click', function(e) {
+			e.preventDefault();
+
+			var data = {
+				name: $('#name').val().trim(),
+				phoneNumber: $('#phoneNumber').val().trim(),
+				email: $('#email').val().trim(),
+				uniqueID: $('#uniqueID').val().trim()
+			}
 
 
-	$.ajax({
-		type: 'POST',
-		data: JSON.stringify(data),
-	    contentType: 'application/json',
-	    url: '/api/new',						
-	    success: function(data) {
-	        console.log('success');
-	        console.log(JSON.stringify(data));
-	    }
-    })
+			$.ajax({
+				type: 'POST',
+				data: JSON.stringify(data),
+			    contentType: 'application/json',
+			    url: '/api/new',						
+			    success: function(data) {
+			        console.log('success');
+			        console.log(JSON.stringify(data));
+			    }
+		    })
 
-})
+		})
